669f8dcae8c28a64d4962ff5cea9b3c851c25225
RiderSathi <ridersathi1805@gmail.com>
2025-06-20 19:03:27 +0530

    string

---
+++ AddBinaryStrings.java
@@ -0,0 +1,29 @@
+class AddBinaryStrings{
+    public String addBinary(String s1, String s2) {
+        StringBuilder result = new StringBuilder();
+        int i = s1.length()-1;
+        int j = s2.length()-1;
+        int carry = 0;
+        while(i>=0||j>=0||carry==1){
+            int sum=carry;
+            if(i>=0){
+                sum+=s1.charAt(i--)-'0';
+            }
+            if(j>=0){
+                sum+=s2.charAt(j--)-'0';
+            }
+            result.append(sum%2);
+            carry=sum/2;
+        }
+        while(result.length()>1&&result.charAt(result.length()-1)=='0'){
+            result.setLength(result.length()-1);
+        }
+        return result.reverse().toString();
+    }
+    public static void main(String[] args) {
+        AddBinaryStrings solution = new AddBinaryStrings();
+        String s1 = "1101";
+        String s2 = "1011";
+        System.out.println(solution.addBinary(s1, s2)); // Output: "11000"
+    }
+}
