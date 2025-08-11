b8cf8090a36784a9e1cc35a8ffd1afe57d6cbc4e
RiderSathi <ridersathi1805@gmail.com>
2025-08-11 21:10:41 +0530

    Two_Pointer_Technique

---
+++ Two_Pointer_Technique/Longestsubstringwithdistinctcharacters.java
@@ -0,0 +1,23 @@
+class Longestsubstringwithdistinctcharacters{
+    public int longestUniqueSubstr(String s){
+        int n=s.length();
+        int res=0;
+        int[] lastIndex=new int[26];
+        for(int i=0;i<26;i++){
+            lastIndex[i]=-1;
+        }
+        int start=0;
+        for(int end=0;end<n;end++){
+            start=Math.max(start,lastIndex[s.charAt(end)-'a']+1);
+            res=Math.max(res,end-start+1);
+            lastIndex[s.charAt(end)-'a']=end;
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        Longestsubstringwithdistinctcharacters obj = new Longestsubstringwithdistinctcharacters();
+        String s = "abcabcbb";
+        int result = obj.longestUniqueSubstr(s);
+        System.out.println("The length of the longest substring with distinct characters is: " + result);
+    }
+}
---
+++ Two_Pointer_Technique/Longestsubstringwithdistinctcharacters.md
@@ -0,0 +1,35 @@
+# Longest Substring with Distinct Characters
+
+## Problem Description
+You are given a string `s`. You have to find the length of the longest substring with all distinct characters.
+
+## Examples
+
+### Example 1
+Input:  
+`s = "geeksforgeeks"`  
+Output:  
+`7`  
+Explanation: `"eksforg"` is the longest substring with all distinct characters.
+
+### Example 2
+Input:  
+`s = "aaa"`  
+Output:  
+`1`  
+Explanation: `"a"` is the longest substring with all distinct characters.
+
+### Example 3
+Input:  
+`s = "abcdefabcbb"`  
+Output:  
+`6`  
+Explanation: The longest substring with all distinct characters is `"abcdef"`, which has a length of 6.
+
+## Constraints
+- 1 ≤ s.size() ≤ 3 × 10⁴  
+- All the characters are in lowercase.
+
+## Expected Complexities
+- **Time Complexity:** O(n)  
+- **Auxiliary Space:** O(1)
