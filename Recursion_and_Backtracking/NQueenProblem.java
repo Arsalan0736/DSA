0c34ce5d70b27658be0a729695266220483a55b3
RiderSathi <ridersathi1805@gmail.com>
2025-09-03 14:45:56 +0530

    Recursion_and_Backtracking

---
+++ Recursion_and_Backtracking/NQueenProblem.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+class NQueenProblem{
+    static void nQueenUtil(int j,int n,ArrayList<Integer> board,boolean[] rows,boolean[] diag1,boolean[] diag2,ArrayList<ArrayList<Integer>> res){
+        if(j>n){
+            res.add(new ArrayList<>(board));
+            return;
+        }
+        for(int i=1;i<=n;i++){
+            if(!rows[i]&&!diag1[i+j]&&!diag2[i-j+n]){
+                rows[i]=diag1[i+j]=diag2[i-j+n]=true;
+                board.add(i);
+                nQueenUtil(j+1,n,board,rows,diag1,diag2,res);
+                board.remove(board.size()-1);
+                rows[i]=diag1[i+j]=diag2[i-j+n]=false;
+            }
+        }
+    }
+    public ArrayList<ArrayList<Integer>> nQueen(int n){
+        ArrayList<ArrayList<Integer>> res=new ArrayList<>();
+        ArrayList<Integer> board=new ArrayList<>();
+        boolean[] rows=new boolean[n+1];
+        boolean[] diag1=new boolean[2*n+1];
+        boolean[] diag2=new boolean[2*n+1];
+        nQueenUtil(1,n,board,rows,diag1,diag2,res);
+        return res;
+    }
+    public static void main(String[] args){
+        NQueenProblem nQueen=new NQueenProblem();
+        int n=4;
+        ArrayList<ArrayList<Integer>> solutions=nQueen.nQueen(n);
+        for(ArrayList<Integer> solution:solutions){
+            System.out.println(solution);
+        }
+    }
+}
